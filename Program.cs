644bc66f30ae3a55714b556f0a16e7c3a33465a1
Teresia Ngigi <tshiks22@outlook.com>
2020-11-14 01:44:51 -0600

    static math pi class

--- Program.cs
+++ Program.cs
@@ -15,18 +15,23 @@ namespace OperatorExercise
             When finished your output should look something like this:*/
 
 
-           
+
             Console.Write("What is the radius of your circle?:  ");
             var radius = double.Parse(Console.ReadLine());
-            //var radius = Convert.ToDouble(Console.ReadLine());
-            var area = AreaOfCircle(radius);
 
-            Console.WriteLine($"The area of a circle with a radius of {radius} is {area} ");
-        }
-        public static double AreaOfCircle(double r)
-        {
-            
-            return Math.PI * r * r; ;
+           // var radius = Convert.ToDouble(Console.ReadLine());
+
+
+            var area = AreaOfCircle(radius);
+            {
+             Console.WriteLine($"The area of a circle with a radius of {radius} is {area} ");
+            }
+
+             static double AreaOfCircle(double r)
+             {
+
+                return Math.PI * r * r; ;
+             } 
         }
         	
     }
