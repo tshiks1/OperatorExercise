a3ca5c39bb69094ca92066657f9e627f3fdea906
Teresia Ngigi <tshiks22@outlook.com>
2020-11-14 00:56:08 -0600

    review

--- Program.cs
+++ Program.cs
@@ -10,16 +10,11 @@ namespace OperatorExercise
             int b = 4;
             int quotient = a / b;
             int remainder = a % b;
+            var product = a * b;
 
             Console.WriteLine($"{a}/{b} is { quotient} remainder {remainder}");
+            Console.WriteLine($"{a}*{b} is { product} remainder {remainder}");
         }
-        /*For division, create two integer variables called a and b. 
-Create a third integer variable and name it quotient (the result of a division) 
-        that stores the result of the division of a and b, 
-        and another integer variable named remainder
-        that stores the remainder (using the % operator). 
-        Write out the results using Console.WriteLine or Console.Write to 
-        write out the results in the following form: if a = 17 and b = 4, print the following:	
-17/4 is 4 remainder 1	*/		
+        
     }
 }
